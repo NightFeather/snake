427b5e4cdb5474dca03b703b0869007f6d4bffeb
NightFeather <ss900328@gmail.com>
2018-02-22 14:35:46 +0800

    Add container types
    config and state

--- include/game.h
+++ include/game.h
@@ -2,6 +2,21 @@
 #ifndef __GAME_H__
 #define __GAME_H__
 
+#include "coord_list.h"
+
+typedef struct {
+  unsigned int delay;
+  int cols, rows;
+  int length;
+  char snake, food;
+} GameConfig;
+
+typedef struct {
+  CoordList snake , food;
+  int score , direction;
+  int key_lock , draw_lock , stopped;
+} GameState;
+
 void game_init();
 void game_start();
 void game_quit();
